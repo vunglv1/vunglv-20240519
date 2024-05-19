62f21f8448b1bc5c5d50527a3d1f33e686ce225e
vunglv <vungbk@gmail.com>
2024-05-19 17:35:03 +0700

    change file

---
+++ cho thue.sql
@@ -0,0 +1,8 @@
+Cho thuê căn hộ THT New City, Hoài Đức, 45M, 1PN, 1PK, có điều hòa, nóng lạnh, đồ cơ bản, tháng 4 triệu
+
+Chính chủ tôi cho thuê căn 1302 tòa A1.1, có điều hòa, nóng lạnh, tủ bếp dưới, bếp từ+hồng ngoại, dàn phơi, tháng 4 triệu. Phone/zalo 0963.541.168
+
+
+
+Xin phép Admin:
+Chính chủ tôi cho thuê căn 1302 tòa A1.1 THT New City, Hoài Đức, trên đường 32, cách Nhổn khoảng 1km - 45M, 1PN, 1PK, có điều hòa, nóng lạnh, tủ bếp dưới, bếp từ+hồng ngoại, dàn phơi, tháng 4 triệu. Phone/zalo 0963.541.168
